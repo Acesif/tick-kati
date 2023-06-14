a81d5658f1429d32cbb06489d91a70bec73005fd
Acesif <asifpalak@gmail.com>
2023-06-15 01:54:15 +0600

    added table to content

--- tick-kati/src/Components/Content.js
+++ tick-kati/src/Components/Content.js
@@ -1,36 +1,31 @@
 import React from 'react'
 
 const Content = () => {
-  return (
+    const addTicket = () => {
+        const overlay = document.querySelector(".overlay")
+        overlay.classList.add("show")
+    }
+    return (
     <div className='content'>
-        <h1>Tickets Type</h1>
+        <div className="header">
+            <h1>Tickets Type</h1>
+            <button onClick={()=>addTicket()} className='btn ticket-add'>Add Ticket Type</button>
+        </div>
+        <div className='overlay'></div>
         <div className='table-container'>
             <table className="table table-dark table-striped-columns">
                 <thead>
                     <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Ticket Type</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                    <th scope="row">1</th>
                     <td>Mark</td>
                     <td>Otto</td>
                     <td>@mdo</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
-                    </tr>
-                    <tr>
-                    <th scope="row">3</th>
-                    <td colspan="2">Larry the Bird</td>
-                    <td>@twitter</td>
                     </tr>
                 </tbody>
             </table>
